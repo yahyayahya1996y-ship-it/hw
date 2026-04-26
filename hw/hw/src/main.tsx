72ac3785da70184fab29405f3d61b07acf55ec17
yahya <yahya.yahya.1996y@gmail.com>
2026-04-26 12:33:19 +0300

    add navbar

--- hw/hw/src/main.tsx
+++ hw/hw/src/main.tsx
@@ -1,10 +1,2 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
